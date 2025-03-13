cf273f52a97eed1dc4750090110df599c827453f
carlosr30110 <carlosr301101@gmail.com>
2025-03-13 02:14:00 -0400

    Corregido bug de rutas

--- base-frontend/src/views/HomeView.vue
+++ base-frontend/src/views/HomeView.vue
@@ -127,7 +127,7 @@
             <li><hr class="dropdown-divider"></li>
             <li><a class="dropdown-item" href="#">ENOC</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">UFC</a></li>
+            <li><router-link class="dropdown-item" to="ParteUfc">UFC</router-link></li>
             <li><hr class="dropdown-divider"></li>
             <li><a class="dropdown-item" href="#">GEA</a></li>
             <li><hr class="dropdown-divider"></li>

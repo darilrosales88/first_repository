3caa20a41f7b9f2cc4af314717b9dc45eb320cb1
int_64 <carlosr301101@gmail.com>
2025-04-26 20:41:25 -0400

    Quitado copia

--- mitrans - Copy/ufc/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import permissions
-
-class IsAdminUFCPermission(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user.groups.filter(name='AdminUFC').exists()
-
-class IsVisualizadorUFCPermission(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user.groups.filter(name__in=['AdminUFC', 'VisualizadorUFC']).exists()

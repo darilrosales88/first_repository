a97dcc6f8f5da5a9800d30246231293531f65d3a
int_64 <carlosr301101@gmail.com>
2025-05-13 23:57:04 -0400

    Componente de Rotacion de vagones Reactivo

--- mitrans/ufc/signals.py
+++ mitrans/ufc/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save,post_save,post_delete
 from django.dispatch import receiver
 from django.utils import timezone
 from .models import (ufc_informe_operativo,                     
@@ -13,6 +13,7 @@ from .models import (ufc_informe_operativo,
 
 
 )
+from django.db.models import Sum
 from django.db import transaction
 
 @receiver(pre_save, sender=ufc_informe_operativo)
@@ -40,5 +41,44 @@ def borrar_registros_antiguos(sender, instance, **kwargs):
                 en_trenes.objects.all().delete()
                 arrastres.objects.all().delete()
                 rotacion_vagones.objects.all().delete()
-
                 
+@receiver(post_delete,sender=vagon_cargado_descargado)
+@receiver(post_save, sender=vagon_cargado_descargado)
+def actualizar_rotacion(sender,instance,**kwargs):
+    """
+    Se ejecuta después de guardar un vagon_cargado_descargado.
+    Busca o crea un registro en rotacion_vagones asociado al tipo de equipo ferroviario.
+    Actualiza: plan_carga, real_carga, plan_rotacion, real_rotacion
+    """
+    print("Se creo un registro cargado")
+    if not instance.tipo_equipo_ferroviario:
+        print("El registro no tiene tipo de equipo ferroviario.")
+        return
+    tipo_equipo = instance.tipo_equipo_ferroviario
+    
+    rotaciones=rotacion_vagones.objects.filter(tipo_equipo_ferroviario=tipo_equipo)
+    for rotacion in rotaciones:
+            actualizar_datos_rotacion(rotacion, tipo_equipo)
+            rotacion.save()
+            print(f"Se actualizó el registro de rotación: {rotacion.id}")
+
+    # Filtrar solo los vagones cargados/descargados para este tipo de equipo
+
+def actualizar_datos_rotacion(rotacion, tipo_equipo):
+
+    # Filtrar los vagones cargados/descargados para este tipo de equipo
+    hoy=timezone.now().date()
+    registros = vagon_cargado_descargado.objects.filter(
+        tipo_equipo_ferroviario=tipo_equipo,
+        operacion='carga',
+        fecha__date=hoy
+    )
+    total_plan_carga =registros.aggregate(Sum("plan_diario_carga_descarga"))['plan_diario_carga_descarga__sum'] or 0
+    total_real_carga = registros.aggregate(Sum('real_carga_descarga'))['real_carga_descarga__sum'] or 0
+    en_servicio = rotacion.en_servicio or 1  # Evitar división por cero
+
+    # Actualizar campos de rotación
+    rotacion.plan_carga = total_plan_carga
+    rotacion.real_carga = total_real_carga
+    rotacion.plan_rotacion = round(total_plan_carga / en_servicio, 2) if en_servicio else 0
+    rotacion.real_rotacion = round(total_real_carga / en_servicio, 2) if en_servicio else 0

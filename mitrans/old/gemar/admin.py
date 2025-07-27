a5a90b8e815e80132418523a102c467f76ff80d5
darilrosales88 <daril.rosales88@gmail.com>
2025-07-26 22:50:37 -0500

    correccion de front en Gemar

--- mitrans/old/gemar/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import gemar_hecho_extraordinario,gemar_parte_hecho_extraordinario
-
-# Register your models here.
-admin.site.register(gemar_parte_hecho_extraordinario)
-admin.site.register(gemar_hecho_extraordinario)

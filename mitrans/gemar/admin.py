27b59e8620c809ec725410d8b3e1a3e002e61441
int_64 <carlosr301101@gmail.com>
2025-08-12 12:39:48 -0400

    logre el merge casi imposible

--- mitrans/gemar/admin.py
+++ mitrans/gemar/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from .models import gemar_hecho_extraordinario,gemar_parte_hecho_extraordinario,gemar_programacion_maniobras,gemar_parte_programacion_maniobras
 
 # Register your models here.
@@ -7,7 +6,3 @@ admin.site.register(gemar_parte_hecho_extraordinario)
 admin.site.register(gemar_hecho_extraordinario)
 admin.site.register(gemar_programacion_maniobras)
 admin.site.register(gemar_parte_programacion_maniobras)
-=======
-
-# Register your models here.
->>>>>>> 84543cf359a24f8c252a51cdb6057de6bd0bf1f3

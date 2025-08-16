c255076f874dbda4a37aea0f6f50ce133c9a2267
int_64 <carlosr301101@gmail.com>
2025-08-16 10:56:00 -0400

    FEATL:Agregada la configuracion de postgresSQL

--- mitrans/mitrans/settings.py
+++ mitrans/mitrans/settings.py
@@ -160,13 +160,28 @@ WSGI_APPLICATION = 'mitrans.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+####BD SQLITE
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+###BD POSTGRESQL
+# Asegurarse de tener instalado el driver psycopg2 o psycopg[binary] para PostgreSQL
+# pip install psycopg[binary]
+DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "client_encoding": 'UTF8',
+            "NAME":"mitrans_db",
+            "USER":"postgres",
+            "PASSWORD":"admin",
+            "HOST":"localhost",
+            "PORT":"5432",         
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
